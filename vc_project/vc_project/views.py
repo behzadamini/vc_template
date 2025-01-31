baaf2b30877cdae94113d9cd909bc8b138bfd575
Behzad Amini <behzadamini.dev@gmail.com>
2025-01-31 08:25:08 +0330

    use template tag for get value from view.py

--- vc_project/vc_project/views.py
+++ vc_project/vc_project/views.py
@@ -2,7 +2,13 @@ from . import urls
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'index.html')
+    content = {
+        'fullname': 'بهزاد امینی',
+        'email': 'BehzadAmini.dev@gmail.com',
+        'phone': '+98912123456',
+        'address': 'تهران، خیابان شهید محمدی، پلاک 123'
+        }
+    return render(request, 'index.html', content)
 
 def blog(request):
     return render(request, 'blog-details.html')
